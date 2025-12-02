aa490e7a712d8203f2bc46ae353ded34eab68a5a
Anton I. Sipos <aisipos@gmail.com>
2025-12-02 14:48:27 -0800

    Add license

--- spy-mode.el
+++ spy-mode.el
@@ -1,4 +1,16 @@
 ;;; spy-mode.el --- Major mode for SPy language -*- lexical-binding: t -*-
+;; spy-mode.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; spy-mode.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with spy-mode.el. If not, see https://www.gnu.org/licenses/
 
 (defface spy-blue
   '((((class color) (min-colors 256) (background light))
